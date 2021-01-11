bd72473bce0c561ac86406dd98d58dcbb8225fa6
hakoai <hakoai64@gmail.com>
2021-01-11 13:53:49 +0900

    fix index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 import * as electron from 'electron';
 export declare const ipcMain: {
-    handle: (channel: string, listener: (event: electron.IpcMainEvent, notify: (...args: unknown[]) => void, cancel: Promise<unknown>, ...args: unknown[]) => Promise<unknown[]>) => void;
+    handle: (channel: string, listener: (event: electron.IpcMainEvent, notify: (...args: unknown[]) => void, cancel: Promise<unknown>, ...args: unknown[]) => Promise<unknown[]>, onError: (e: unknown) => void) => void;
     removeAllListeners: (channel: string) => void;
 };
 export declare const ipcRenderer: {
